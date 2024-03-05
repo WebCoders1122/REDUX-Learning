da25c028ac7e108e83d5f1577302ba5fd50c8c35
webcoders <webcoders1122@gmail.com>
2024-03-05 17:26:21 +0500

    initial code with uncomplete setup

---
+++ redux-api/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Products } from "./features/products/Products";
+import "./App.css";
+
+function App() {
+  return <div className='App'></div>;
+}
+
+export default App;
